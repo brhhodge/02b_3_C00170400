3b04814d2156755f93e577eb4f28d590edd6303f
Brian Hodge <brianrhodge@gmail.com>
2021-04-20 22:01:21 -0500

    Added .csproj, obj, and .vscode files

---
+++ Dirt.cs
@@ -0,0 +1,51 @@
+using System;
+
+namespace _02b_3_C00170400
+{
+    class Dirt
+    {
+        private double gramsOfSand;
+        private double gramsOfClay;
+
+        public Dirt()
+        {
+            this.gramsOfSand = 1.0;
+            this.gramsOfClay = 1.0;
+        }
+        public Dirt(double s, double c)
+        {
+            if (s >= 0){
+                this.gramsOfSand = s;
+            }
+            if (c >= 0){
+                this.gramsOfClay = c;
+            }
+        }
+
+        public void IncreaseSand()
+        {
+            this.gramsOfSand = this.gramsOfSand + 1;
+        }
+        public void DecreaseSand()
+        {
+            this.gramsOfSand = this.gramsOfSand - 1;
+        }
+        public double GetSand()
+        {
+            return gramsOfSand;
+        }
+        public void IncreaseClay()
+        {
+            this.gramsOfClay = this.gramsOfClay + 1;
+        }
+        public void DecreaseClay()
+        {
+            this.gramsOfClay = this.gramsOfClay - 1;
+        }
+        public double GetClay()
+        {
+            return gramsOfClay;
+        }
+
+    }
+}
